afc13f96f99e26b8e7e06338398dd5b0dea66fd5
Adrian Mudzwiti <54492734+AdrianM10@users.noreply.github.com>
2024-12-02 19:24:56 +0200

    Author script to create DynamoDB Table locally

---
+++ dynamo_db_local/create_ddb_table.py
@@ -0,0 +1,50 @@
+import boto3
+from botocore.exceptions import ClientError
+
+
+def main():
+
+    # Create DynamoDB Client
+    dynamodb = boto3.resource(
+        "dynamodb",
+        endpoint_url="http://localhost:7001",
+        region_name="af-south",
+        aws_access_key_id="myid",
+        aws_secret_access_key="myaccesskey",
+    )
+
+    create_dynamodb_table(dynamodb)
+
+
+def create_dynamodb_table(dynamodb):
+
+    # Check if DynamoDB Table exists
+    table_name = "PlayersOnly"
+
+    existing_tables = dynamodb.meta.client.list_tables()["TableNames"]
+
+    if table_name not in existing_tables:
+
+        try:
+
+            # Create DynamoDB Table
+
+            table = dynamodb.create_table(
+                TableName=table_name,
+                KeySchema=[{"AttributeName": "PlayerId", "KeyType": "HASH"}],
+                AttributeDefinitions=[
+                    {"AttributeName": "PlayerId", "AttributeType": "S"}],
+                BillingMode='PAY_PER_REQUEST'
+            )
+
+            print(f"Successfully created table: {table.table_name}")
+
+        except ClientError as e:
+            print(e)
+
+    else:
+        print(f"Table '{table_name}' already exists.")
+
+
+if __name__ == "__main__":
+    main()
