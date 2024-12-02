e6a8151ce81700aec1bc7db3a7a1dcdc2abcc4ab
Adrian Mudzwiti <54492734+AdrianM10@users.noreply.github.com>
2024-12-02 21:16:30 +0200

    Add path parameter to retrieve a single player by id

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ handler = Mangum(app)
 
 
 class Player(BaseModel):
+    id: str
     name: str
     country: str
     # date_of_birth: date
@@ -22,20 +23,6 @@ def root():
     return {"statusCode": 200, "body": "Hello Player! Welcome To The Beautiful Game"}
 
 
-def get_local_dynamodb_table():
-    table_name = "Players"
-    return boto3.resource("dynamodb",
-                          endpoint_url="http://localhost:7001",
-                          region_name="af-south",
-                          aws_access_key_id="myid",
-                          aws_secret_access_key="myaccesskey").Table(table_name)
-
-
-def get_dynamoddb_table():
-    table_name = "Players"
-    return boto3.resource("dynamodb").Table(table_name)
-
-# @app.get("/players/{}")
 
 
 @app.post("/players/")
@@ -55,7 +42,28 @@ async def add_player(player: Player):
     table.put_item(Item=item)
     return {"player": item}
 
+@app.get("/players/{id}")
+async def get_player(id: str):
+    # Retrieve player data by id from table
+    table = get_local_dynamodb_table()
+    response = table.get_item(Key={"PlayerId": id})
+    item = response.get("Item")
+    return item
+
 
 # @app.put
 
 # @app.delete
+
+def get_local_dynamodb_table():
+    table_name = "Players"
+    return boto3.resource("dynamodb",
+                          endpoint_url="http://localhost:7001",
+                          region_name="af-south",
+                          aws_access_key_id="myid",
+                          aws_secret_access_key="myaccesskey").Table(table_name)
+
+
+def get_dynamoddb_table():
+    table_name = "Players"
+    return boto3.resource("dynamodb").Table(table_name)
