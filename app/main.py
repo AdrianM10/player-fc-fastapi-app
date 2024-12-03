770f3f853b48b3d0651f35de253075931adc7b74
Adrian Mudzwiti <54492734+AdrianM10@users.noreply.github.com>
2024-12-03 06:58:18 +0200

    Store date object as string

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ local_development = True
 class Player(BaseModel):
     name: str
     country: str
-    # date_of_birth: date
+    date_of_birth: date
     team: str
     position: str
     shirt_number: int
@@ -27,22 +27,26 @@ def root():
 
 @app.post("/players/")
 async def add_player(player: Player):
+    
     player_id = uuid.uuid5(uuid.NAMESPACE_DNS, f"{player.name}-{player.country}")
 
     item = {
         "id": str(player_id),
         "name": player.name,
         "country": player.country,
-        # "date_of_birth": player.date_of_birth,
+        "date_of_birth": player.date_of_birth.isoformat(),
         "team": player.team,
         "position": player.position,
         "shirt_number": player.shirt_number,
     }
 
     # Add player to Dynamo DB Table
-    table = get_dynamoddb_table()
-    table.put_item(Item=item)
-    return {"player_id": item["id"], "player_name": item["name"]}
+    try:
+        table = get_dynamoddb_table()
+        table.put_item(Item=item)
+        return {"player_id": item["id"], "player_name": item["name"]}
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 @app.get("/players/{id}")
@@ -57,15 +61,6 @@ async def get_player(id: str):
 # @app.put
 
 # @app.delete
-
-# def get_local_dynamodb_table():
-#     table_name = "Players"
-#     return boto3.resource("dynamodb",
-#                           endpoint_url="http://localhost:7001",
-#                           region_name="af-south",
-#                           aws_access_key_id="myid",
-#                           aws_secret_access_key="myaccesskey").Table(table_name)
-
 
 def get_dynamoddb_table():
     table_name = "Players"
