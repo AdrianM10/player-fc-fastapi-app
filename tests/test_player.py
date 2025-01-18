617fd02f4c5a60974cb51305940b42ade38fd824
Adrian Mudzwiti <54492734+AdrianM10@users.noreply.github.com>
2025-01-18 16:53:31 +0200

    Restructure sequence of functions, test updating player data

--- tests/test_player.py
+++ tests/test_player.py
@@ -8,15 +8,6 @@ from app.main import app
 client = TestClient(app)
 
 
-def test_root():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {
-        "message": "Welcome to the Players FC API!",
-        "description": "More than just a Game"
-    }
-
-
 @pytest.fixture(scope="module")
 def aws_credentials():
     """Mocked AWS Credentials for moto."""
@@ -43,6 +34,15 @@ def dynamodb_table(aws_credentials):
         yield table
 
 
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "Welcome to the Players FC API!",
+        "description": "More than just a Game"
+    }
+
+
 def test_create_player(dynamodb_table):
     player = {
         "id": "4d6d8412-c021-52bc-8c47-feed545b0ced",
@@ -61,4 +61,39 @@ def test_create_player(dynamodb_table):
 
     assert 'Item' in response
     assert response['ResponseMetadata']['HTTPStatusCode'] == 200
-    assert response['Item']['id'] == '4d6d8412-c021-52bc-8c47-feed545b0ced' 
+    assert response['Item']['id'] == '4d6d8412-c021-52bc-8c47-feed545b0ced'
+    assert response['Item']['name'] == 'Christopher Nkunku'
+
+
+def test_update_player(dynamodb_table):
+
+    id = "4d6d8412-c021-52bc-8c47-feed545b0ced"
+
+    player_data = {
+        "id": id,
+        "name": "Christopher Nkunku",
+        "country": "France",
+        "date_of_birth": "1997-11-14",
+        "team": "Chelsea",
+        "position": "Forward",
+        "club_number": 18,
+        "national_team_number": 12
+    }
+
+    dynamodb_table.put_item(Item=player_data)
+    response = dynamodb_table.get_item(
+        Key={'id': '4d6d8412-c021-52bc-8c47-feed545b0ced'})
+
+
+    update_player_data = {
+        "team": "Bayern Munich"
+    }
+
+    dynamodb_table.update_item(
+        Key={'id': id},
+        UpdateExpression='SET team = :team',
+        ExpressionAttributeValues={':team': update_player_data['team']}
+    )
+
+    response = dynamodb_table.get_item(Key={'id': id})['Item']
+    assert response['team']  == 'Bayern Munich'
