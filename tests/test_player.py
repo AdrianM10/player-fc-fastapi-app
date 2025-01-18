0ce9828713ea713615e19243dd3b2d811dd195c0
Adrian Mudzwiti <54492734+AdrianM10@users.noreply.github.com>
2025-01-18 15:42:27 +0200

    Add pytest fixtures to mock aws credentials, dynamodb. Test creating player

--- tests/test_player.py
+++ tests/test_player.py
@@ -1,9 +1,12 @@
 import pytest
+import boto3
+import os
 from fastapi.testclient import TestClient
-from datetime import date
+from moto import mock_aws
 from app.main import app
 
 client = TestClient(app)
+
 
 def test_root():
     response = client.get("/")
@@ -12,3 +15,50 @@ def test_root():
         "message": "Welcome to the Players FC API!",
         "description": "More than just a Game"
     }
+
+
+@pytest.fixture(scope="module")
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"
+    os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "af-south-1"
+
+
+@pytest.fixture
+def dynamodb_table(aws_credentials):
+    """Mock DynamoDB table for testing"""
+    dynamodb = boto3.resource('dynamodb', region_name='af-south-1')
+
+    with mock_aws():
+        table = dynamodb.create_table(
+            TableName="Players",
+            KeySchema=[{"AttributeName": "id", "KeyType": "HASH"}],
+            AttributeDefinitions=[
+                {"AttributeName": "id", "AttributeType": "S"}],
+            BillingMode='PAY_PER_REQUEST'
+        )
+        yield table
+
+
+def test_create_player(dynamodb_table):
+    player = {
+        "id": "4d6d8412-c021-52bc-8c47-feed545b0ced",
+        "name": "Christopher Nkunku",
+        "country": "France",
+        "date_of_birth": "1997-11-14",
+        "team": "Chelsea",
+        "position": "Forward",
+        "club_number": 18,
+        "national_team_number": 12
+    }
+
+    dynamodb_table.put_item(Item=player)
+    response = dynamodb_table.get_item(
+        Key={'id': '4d6d8412-c021-52bc-8c47-feed545b0ced'})
+
+    assert 'Item' in response
+    assert response['ResponseMetadata']['HTTPStatusCode'] == 200
+    assert response['Item']['id'] == '4d6d8412-c021-52bc-8c47-feed545b0ced' 
